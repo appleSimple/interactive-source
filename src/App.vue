cf71aa1fa6365766a7421dbd12964ed2d87f804a
jiyeon.kim <jiyeon.kim@ikoob.com>
2022-07-06 18:22:36 +0900

    IMP: 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,17 @@
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
+  <SideBar />
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {},
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/SideBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="sideBar">dddd</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  name: "SideBar",
+})
+export default class SideBar extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+#sideBar {
+  width: 30%;
+  border: 1px solid red;
+}
+</style>
